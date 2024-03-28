52611d1dc7681a7788d0b8f023b7dce6565bfe62
peruibeloko <cmendonca32@gmail.com>
2024-03-28 19:59:57 -0300

    tco for factorial

--- factorial.hs
+++ factorial.hs
@@ -1,6 +1,9 @@
 fact :: Int -> Int
-fact 0 = 1
-fact n = n * fact (n - 1)
+fact n = factInternal n 1
+
+factInternal :: Int -> Int -> Int
+factInternal 0 acc = acc
+factInternal n acc = fact (n - 1, acc * n)
 
 main :: IO ()
 main = do
