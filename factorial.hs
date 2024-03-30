0b92e1e27425ddfcadf8073423e6ff143e180c16
Carlinhos <cmendonca32@gmail.com>
2024-03-30 16:21:56 -0300

    Update factorial.hs

--- factorial.hs
+++ factorial.hs
@@ -3,7 +3,7 @@ fact n = factInternal n 1
 
 factInternal :: Int -> Int -> Int
 factInternal 0 acc = acc
-factInternal n acc = fact (n - 1, acc * n)
+factInternal n acc = factInternal (n - 1, acc * n)
 
 main :: IO ()
 main = do
